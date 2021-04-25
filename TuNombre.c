ce415a4786502e89aaa159d31278957a12a7eb2e
Camila Susana Alvarez Brito <67988428+CamiSusana@users.noreply.github.com>
2021-04-24 21:59:37 -0300

    Add files via upload

---
+++ TuNombre.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+
+int main () 
+{
+    char nombre[30]; 
+    printf ("Introduce tu nombre "); 
+    scanf("%s",nombre);
+    printf("\n\n\n%s",nombre); 
+    
+     return 0; 
+    
+}
